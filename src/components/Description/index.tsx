eeabaf315d08894426bf5a4f6b0af95702d7e2fa
dandan <dandanveiga1@gmail.com>
2022-07-21 19:07:31 -0300

    Changed Text

--- src/components/Description/index.tsx
+++ src/components/Description/index.tsx
@@ -3,8 +3,8 @@ import styles from "./Description.module.scss";
 export default function Description() {
    return (
       <p className={styles.text}>
-         I'm a front end programmer and I do drawings as a hobby. I live on Brazil and I love to learn and try new things.
-         Here you can see some of my social media.
+         Hi! I am a front-end programmer and I do drawings as a hobby. I live on Brazil and I love to learn and try new things.
+         Here you can see some of my social medias.
       </p>
    );
 }
